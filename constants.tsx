4b61a7e6eca3ea56a48762b91dcce868666a1fc3
arxivius <148529070+arXiVius@users.noreply.github.com>
2025-12-30 15:16:43 +0700

    added project

--- constants.tsx
+++ constants.tsx
@@ -6,4 +6,5 @@ export const PROJECTS: Project[] = [
   { id: '2', name: 'dumpShyt', description: 'where my last 2 brain cells live', href: 'https://dumpshyt.vercel.app/', active: true, status: 'LIVE' },
   { id: '3', name: 'yuhOrNuh', description: '50/50 chance to ruin your day', href: 'https://yuhornuh.vercel.app/', active: true, status: 'LIVE' },
   { id: '4', name: 'thys', description: 'literally just a div with a border(not really but whatever)', href: 'https://thys.vercel.app/', active: true, status: 'LIVE' },
+  { id: '4', name: 'who', description: 'makes sorting people faster(i guess)', href: 'https://whosimply.vercel.app/', active: true, status: 'LIVE' },
 ];
